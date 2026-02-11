b4fbd31fdf692677854d29b797bc5872ce89ea23
WeixiMa <mavc@fastmail.com>
2026-02-11 14:09:13 -0800

    push

--- learn/overview/main.ts
+++ learn/overview/main.ts
@@ -35,18 +35,21 @@
         dialog: [
             message(
                 "W",
-                "Here we write programs for machine learning with PyPie. " +
-                "We assume readers are familiar with high school math, and leave having built a linear regression model, a CNN, and an RNN.\n\n" +
-                "Through these models, we show two principles that apply to real-world programming: clarity and elegance. " +
-                "Clarity comes from PyPie's type system. It ensures predictable behavior and catches bugs before our code runs. " +
-                "Elegance comes from abstractions: reusing core concepts while hiding low-level mechanics. " +
-                "This lets we focus on creative and enjoyable work, trusting recurring patterns to handle the implementation details.\n\n" +
-                "We believe in \"teaching by asking questions\" and \"learning it by building it.\" " +
-                "Each chapter is organized as a dialog between a teacher and a student, containing questions, answers, and programs. " + 
-                "To best internalize this tutorial, type in and run these programs in PyPie, as the conversation progresses.\n\n" +
-                "PyPie is inspired by The Little Learner written by Friedman and Mendhekar, which gives the logical foundation " +
-                "of PyPie's programming system. " + 
-                "The CNN and RNN examples originate from Dive into Deep Learning, which dives into the detailed mathematics underlying these models."
+                "This tutorial trains programmers.\n\n" +
+                "By working through three models--linear regression, CNN, and RNN--" +
+                "we show how to write programs for machine learning, with types and abstraction. " +
+                "Types give clarity to our programs and catch bugs before the programs run. " +
+                "Abstraction lets us reuse core concepts while hiding low-level mechanics. " +
+                "This allows us to focus on the creative and enjoyable parts, trusting recurring patterns to handle the less interesting details.\n\n" +
+                "These fundamental principles are simple to demonstrate and require little prior background: " +
+                "first-time programmers are welcome and high school math is sufficient. " +
+                "We, however, ask for patience and hands-on practice: fully explore every chapter and learn by building. " +
+                "Each chapter is a dialog between a teacher and a student, with questions, answers, and programs. " + 
+                "As you are enjoying the dialogs, internalize them by typing in and running the programs in PyPie.\n\n" +
+                "PyPie is inspired by The Little Learner written by Friedman and Mendhekar, " +
+                "where readers will find a logical foundation. " + 
+                "The CNN and RNN examples originate from Dive into Deep Learning, " +
+                "which offers the math underlying these models and more."
             ),
         ],
     });
