381abf94e433d4836675be8d843cfd2dca34f6f4
WeixiMa <mavc@fastmail.com>
2026-02-11 11:38:45 -0800

    push

--- learn/overview/main.ts
+++ learn/overview/main.ts
@@ -35,14 +35,18 @@
         dialog: [
             message(
                 "W",
-                "This tutorial is about writing programs for machine learning using `pypie`. "+
-                "We assume readers come with a high school math background and " +
-                "walk away after building a linear regression model, a CNN, and an RNN.\n\n" +
-                "More than these three models, we help readers to internalize the underlying principles that apply to real world problems: clairity and elegance.\n\n" +
-                "Clarity is achieved by types. Types serve as annotations during authoring, to remind us the behavior of each module. " +
-                "Types serve as contracts before programs run, to detect bugs early.\n\n" +
-                "Elegance is achieved by abstractions. Abstractions enable reusing the core concepts while ignoring implementation details. " +
-                "So programming becomes a pleasing experience that puts some creativity on top of a recurring pattern.\n\n"
+                "Welcome. Here we write programs for machine learning with PyPie. " +
+                "We assume readers start with knowing high school math, and leave having built a linear regression model, a CNN, and an RNN.\n\n" +
+                "Through these models, we show two principles that generalize to real-world programming: clarity and elegance. " +
+                "Clarity comes from PyPie's type system. It ensures predictable behavior and catches bugs before our code runs. " +
+                "Elegance comes from abstractions. They let us reuse core concepts while hiding low-level mechanics. " +
+                "So we can focus on the creative and enjoyable work while trusting some recurring patterns to handle the implementation details.\n\n" +
+                "We believe in \"teaching by asking questions\" and \"learning it by building it.\" " +
+                "Each chapter is organized as a dialog between a teacher and a student, contains questions, answers, and program examples." + 
+                "To best internalize this tutorial, type in and run these examples in PyPie, as the conversation progreses.\n\n" +
+                "PyPie is inspired by the The Little Learner written by Friedman and Mendhekar, which gives the logical foundation " +
+                "of PyPie's programming system. " + 
+                "The CNN and RNN examples originate from Dive into Deep Learning, which dives into the detailed mathematics underlying these models."
             ),
         ],
     });
